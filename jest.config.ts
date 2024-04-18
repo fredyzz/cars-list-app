8ec89634d8fae441b91c1e294a783a1cf8352504
fredyzz <17734680+fredyzz@users.noreply.github.com>
2024-04-18 21:48:40 +0200

    Install and configure MSW

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,10 @@
-module.exports = {
-    preset: "ts-jest",
-    testEnvironment: "jsdom",
-    testMatch: ["**/*.test.ts", "**/*.test.tsx"],
-    testPathIgnorePatterns: ["/node_modules/", "settings.ts"],
-  };
+export default {
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  moduleNameMapper: {
+    "\\.(gif|ttf|eot|svg|png)$": "<rootDir>/test/__mocks__/fileMock.js",
+  },
+  testMatch: ["**/*.test.ts", "**/*.test.tsx"],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+};
